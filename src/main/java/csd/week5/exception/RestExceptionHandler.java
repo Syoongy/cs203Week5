6fdb8eb664bcc3cdf9b5b3739f583406a86c4f47
Syoongy <mastersyong@gmail.com>
2022-09-13 18:14:10 +0800

    refactor(restexceptionhandler.java): enable exception handler

--- src/main/java/csd/week5/exception/RestExceptionHandler.java
+++ src/main/java/csd/week5/exception/RestExceptionHandler.java
@@ -20,7 +20,8 @@ import java.util.Map;
 /**
  * Centralize exception handling in this class.
  * 
- * TODO: Activity 1 - Uncomment the line below (@ControllerAdvice) to designate that 
+ * TODO: Activity 1 - Uncomment the line below (@ControllerAdvice) to designate
+ * that
  * this class is a central place to handle exceptions across your app
  */
 @ControllerAdvice
@@ -37,17 +38,16 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
      * controller method's arguments.
      */
     @Override
-    protected ResponseEntity<Object>
-    handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                 HttpHeaders headers,
-                                 HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
 
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", new Date());
         body.put("status", status.value());
         body.put("error", status.getReasonPhrase());
         String message = "";
-        for (ObjectError objectError : ex.getBindingResult().getAllErrors()){
+        for (ObjectError objectError : ex.getBindingResult().getAllErrors()) {
             message = message + objectError.getDefaultMessage();
         }
         body.put("message", message);
@@ -57,14 +57,14 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     /**
-     * Handle the case in which arguments for controller's methods did not match the type.
+     * Handle the case in which arguments for controller's methods did not match the
+     * type.
      * E.g., bookId passed in is not a number
      */
-    /*
+
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public void handleTypeMismatch(HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.BAD_REQUEST.value());
-    }*/
-
+    }
 
 }
